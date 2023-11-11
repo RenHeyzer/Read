6705b2c715ab442f00a1bbe71cc3c69868f268dd
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-12 01:45:02 +0600

    Mapper for map book chapter model from data to domain

---
+++ app/src/main/java/com/example/read/utils/mapper/ChapterMapper.kt
@@ -0,0 +1,22 @@
+package com.example.read.utils.mapper
+
+import com.example.read.feature_detail.data.remote.dtos.ChapterDto
+import com.example.read.feature_detail.domain.models.Chapter
+import javax.inject.Inject
+
+class ChapterMapper @Inject constructor() : Mapper<ChapterDto, Chapter> {
+
+    override fun to(model: ChapterDto) = Chapter(
+        tome = model.tome,
+        number = model.number,
+        title = model.title,
+        releaseDate = model.releaseDate
+    )
+
+    override fun from(model: Chapter) = ChapterDto(
+        tome = model.tome,
+        number = model.number,
+        title = model.title,
+        releaseDate = model.releaseDate
+    )
+}
