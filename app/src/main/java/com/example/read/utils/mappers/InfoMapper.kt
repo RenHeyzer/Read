ce87f21562a098a57b4c905ef03874289a955eef
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-12 03:09:28 +0600

    Model of info about book

---
+++ app/src/main/java/com/example/read/utils/mappers/InfoMapper.kt
@@ -0,0 +1,44 @@
+package com.example.read.utils.mappers
+
+import com.example.read.feature_detail.data.remote.dtos.ChapterDto
+import com.example.read.feature_detail.data.remote.dtos.InfoDto
+import com.example.read.feature_detail.domain.models.Chapter
+import com.example.read.feature_detail.domain.models.Info
+import javax.inject.Inject
+
+class InfoMapper @Inject constructor(
+    private val chapterMapper: Mapper<ChapterDto, Chapter>
+) : Mapper<InfoDto, Info> {
+
+    override fun to(model: InfoDto) = Info(
+        id = model.id,
+        title = model.title,
+        description = model.description,
+        coverImage = model.coverImage,
+        type = model.type,
+        status = model.status,
+        rating = model.rating,
+        genres = model.genres,
+        releaseYear = model.releaseYear,
+        chapters = model.chapters?.map {
+            chapterMapper.to(it)
+        },
+        numberOfChapters = model.numberOfChapters
+    )
+
+    override fun from(model: Info) = InfoDto(
+        id = model.id,
+        title = model.title,
+        description = model.description,
+        coverImage = model.coverImage,
+        type = model.type,
+        status = model.status,
+        rating = model.rating,
+        genres = model.genres,
+        releaseYear = model.releaseYear,
+        chapters = model.chapters?.map {
+            chapterMapper.from(it)
+        },
+        numberOfChapters = model.numberOfChapters
+    )
+}
