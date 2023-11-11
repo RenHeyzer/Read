6f66142fb96b490458d3c724f544d2aae8431743
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-12 04:37:43 +0600

    Initialize Supabase Client

--- app/src/main/java/com/example/read/di/AppModule.kt
+++ app/src/main/java/com/example/read/di/AppModule.kt
@@ -5,6 +5,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
 import javax.inject.Singleton
 
 @Module
@@ -14,4 +16,13 @@ object AppModule {
     @Provides
     @Singleton
     fun provideAppDispatchers(): AppDispatchers = AppDispatchers()
+
+    @Provides
+    @Singleton
+    fun providedSupabaseClient() = createSupabaseClient(
+        supabaseUrl = "https://ckipllpkakxblzzpkjnj.supabase.co",
+        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNraXBsbHBrYWt4Ymx6enBram5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkxOTM1MDQsImV4cCI6MjAxNDc2OTUwNH0.geF38px8D4TddS5Tlp3NYpGxqwlKoucRRlEUY3MZqfE"
+    ) {
+        install(Postgrest)
+    }
 }
