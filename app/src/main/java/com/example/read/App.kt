0bad42e6ccd8ad33d0c4c821048d17fb9e879f36
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-12 01:25:35 +0600

    initialized Parse

--- app/src/main/java/com/example/read/App.kt
+++ app/src/main/java/com/example/read/App.kt
@@ -1,7 +1,20 @@
 package com.example.read
 
 import android.app.Application
+import com.parse.Parse
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Parse.initialize(
+            Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_application_id))
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        )
+    }
+}
