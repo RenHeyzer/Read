c43ec1d439f8ce4e7c2d6a4fa2c6ffd5864a20aa
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-12 02:35:15 +0600

    Added new screens "Catalog", "Profile", "Detail"

--- app/src/main/java/com/example/read/main/presentation/Screen.kt
+++ app/src/main/java/com/example/read/main/presentation/Screen.kt
@@ -1,15 +1,12 @@
 package com.example.read.main.presentation
 
 import androidx.annotation.StringRes
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import com.example.read.R
 
 sealed class Screen(val route: String, @StringRes val resourceId: Int) {
-    object Books : Screen("books", R.string.books)
-    object Bookmarks : Screen("bookmarks", R.string.bookmarks)
-    object Account : Screen("account", R.string.account)
+    data object Home : Screen("home", R.string.screen_home)
+    data object Catalog : Screen("catalog", R.string.screen_catalog)
+    data object Bookmarks : Screen("bookmarks", R.string.screen_bookmarks)
+    data object Profile : Screen("profile", R.string.screen_profile)
+    data object Detail : Screen("detail", R.string.screen_detail)
 }
