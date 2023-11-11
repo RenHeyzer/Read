fcd6411ab37c9ed3e19f3bc292f2a06b931e7b48
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-12 03:07:25 +0600

    Design of Bookmark Screen

---
+++ app/src/main/java/com/example/read/feature_bookmarks/presentation/screens/BookmarksScreen.kt
@@ -0,0 +1,16 @@
+package com.example.read.feature_bookmarks.presentation.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun BookmarksScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController = rememberNavController(),
+) {
+    //TODO: Make up the design for Bookmarks Screen
+    Box(modifier = modifier)
+}
