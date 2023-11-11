5c9bac55d6b2dc2e5d898d9e41d6177087bf6b65
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-12 03:09:48 +0600

    File for gradient colors

---
+++ app/src/main/java/com/example/read/ui/theme/Gradient.kt
@@ -0,0 +1,24 @@
+package com.example.read.ui.theme
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+val PurpleVertical = Brush.verticalGradient(
+    listOf(Purple90, DarkPurple90)
+)
+
+val DarkPurpleVertical = Brush.verticalGradient(
+    listOf(
+        DarkPurple,
+        DarkPurple50,
+        DarkPurple50,
+        DarkPurple
+    )
+)
+
+val WhiteVertical = Brush.verticalGradient(
+    listOf(
+        Color.White,
+        White80
+    )
+)
