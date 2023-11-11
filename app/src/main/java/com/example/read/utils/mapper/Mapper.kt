8ede5c1a43da4f62e3f4073e94e6d14436d9cbff
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-12 02:25:30 +0600

    Renamed function for map(from, to)

--- app/src/main/java/com/example/read/utils/mapper/Mapper.kt
+++ app/src/main/java/com/example/read/utils/mapper/Mapper.kt
@@ -2,9 +2,8 @@ package com.example.read.utils.mapper
 
 interface Mapper<F : Mappable, T : Mappable> {
 
-    fun toModel(model: F): T
-
-    fun fromModel(model: T): F
+    fun to(model: F): T
+    fun from(model: T): F
 }
 
 interface Mappable
