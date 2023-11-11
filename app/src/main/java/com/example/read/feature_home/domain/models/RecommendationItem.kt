36b2c5c46c630ef0acb67600f7d4f2fa292794ff
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-12 02:49:41 +0600

    Model of Recommendation

---
+++ app/src/main/java/com/example/read/feature_home/domain/models/RecommendationItem.kt
@@ -0,0 +1,10 @@
+package com.example.read.feature_home.domain.models
+
+import com.example.read.utils.mapper.Mappable
+
+data class RecommendationItem(
+    val id: String? = null,
+    val title: String? = null,
+    val coverImage: String? = null,
+    val posterImage: String? = null
+): Mappable
