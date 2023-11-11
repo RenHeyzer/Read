f737d3c6dbc362f1e7ecaba20f33936f57d489b1
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-12 02:37:35 +0600

    Binds InfoMapper, ChapterMapper, RecommendationItem

--- app/src/main/java/com/example/read/di/MapperModule.kt
+++ app/src/main/java/com/example/read/di/MapperModule.kt
@@ -1,9 +1,18 @@
 package com.example.read.di
 
-import com.example.read.books.data.remote.dtos.ItemDto
-import com.example.read.books.domain.models.Item
+import com.example.read.feature_detail.data.remote.dtos.ChapterDto
+import com.example.read.feature_detail.data.remote.dtos.InfoDto
+import com.example.read.feature_detail.domain.models.Chapter
+import com.example.read.feature_detail.domain.models.Info
+import com.example.read.feature_home.data.remote.dtos.BookItemDto
+import com.example.read.feature_home.data.remote.dtos.RecommendationItemDto
+import com.example.read.feature_home.domain.models.BookItem
+import com.example.read.feature_home.domain.models.RecommendationItem
+import com.example.read.utils.mapper.BookItemMapper
+import com.example.read.utils.mapper.ChapterMapper
+import com.example.read.utils.mapper.InfoMapper
 import com.example.read.utils.mapper.Mapper
-import com.example.read.utils.mapper.dtos.ItemMapper
+import com.example.read.utils.mapper.RecommendationItemMapper
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -14,5 +23,14 @@ import dagger.hilt.components.SingletonComponent
 interface MapperModule {
 
     @Binds
-    fun bindItemMapper(mapper: ItemMapper): Mapper<ItemDto, Item>
+    fun bindBookItemMapper(mapper: BookItemMapper): Mapper<BookItemDto, BookItem>
+
+    @Binds
+    fun bindInfoMapper(mapper: InfoMapper): Mapper<InfoDto, Info>
+
+    @Binds
+    fun bindChapterMapper(mapper: ChapterMapper): Mapper<ChapterDto, Chapter>
+
+    @Binds
+    fun bindRecommendationItemMapper(mapper: RecommendationItemMapper): Mapper<RecommendationItemDto, RecommendationItem>
 }
