1d5243649a7544e7a142599672713c0daac867e8
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-12 20:03:16 +0600

    Paginate request for get books

--- app/src/main/java/com/example/read/feature_home/data/paging/BooksPagingSource.kt
+++ app/src/main/java/com/example/read/feature_home/data/paging/BooksPagingSource.kt
@@ -1,30 +1,32 @@
 package com.example.read.feature_home.data.paging
+
 import android.util.Log
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import com.example.read.feature_home.data.remote.dtos.BookItemDto
 import com.example.read.feature_home.data.remote.sources.books.BooksRemoteDataSource
-import com.parse.ParseException
-import com.parse.ParseObject
+import io.github.jan.supabase.exceptions.HttpRequestException
+import io.github.jan.supabase.exceptions.RestException
+import io.ktor.client.plugins.HttpRequestTimeoutException
 import java.io.IOException
 
 class BooksPagingSource(
     private val booksRemoteDataSource: BooksRemoteDataSource,
     private val searchQuery: String,
-) : PagingSource<Int, ParseObject>() {
-    override fun getRefreshKey(state: PagingState<Int, ParseObject>): Int? =
+) : PagingSource<Long, BookItemDto>() {
+    override fun getRefreshKey(state: PagingState<Long, BookItemDto>): Long? =
         null
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ParseObject> {
+    override suspend fun load(params: LoadParams<Long>): LoadResult<Long, BookItemDto> {
         return try {
             val currentPage = params.key ?: 0
 
             Log.e("key", currentPage.toString())
 
             val comics =
-                booksRemoteDataSource.fetchComics(
-                    limit = params.loadSize,
-                    skip = currentPage,
-                    searchQuery
+                booksRemoteDataSource.getBooks(
+                    offset = currentPage,
+                    limit = params.loadSize.toLong(),
                 )
 
             val nextKey = if (comics.isNotEmpty()) currentPage + comics.size else null
@@ -37,7 +39,13 @@ class BooksPagingSource(
         } catch (e: IOException) {
             e.printStackTrace()
             LoadResult.Error(e)
-        } catch (e: ParseException) {
+        } catch (e: RestException) {
+            e.printStackTrace()
+            LoadResult.Error(e)
+        } catch (e: HttpRequestTimeoutException) {
+            e.printStackTrace()
+            LoadResult.Error(e)
+        } catch (e: HttpRequestException) {
             e.printStackTrace()
             LoadResult.Error(e)
         } catch (e: Exception) {
