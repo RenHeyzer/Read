04f4308e55e07c6ef3ecb0e83d6c4bbc88ec5d90
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-12 19:55:42 +0600

    Request for get paginate books from Supabase

--- app/src/main/java/com/example/read/feature_home/data/remote/sources/books/BooksRemoteDataSource.kt
+++ app/src/main/java/com/example/read/feature_home/data/remote/sources/books/BooksRemoteDataSource.kt
@@ -1,13 +1,8 @@
 package com.example.read.feature_home.data.remote.sources.books
 
 import com.example.read.feature_home.data.remote.dtos.BookItemDto
-import com.parse.ParseObject
 
 interface BooksRemoteDataSource {
 
-    suspend fun fetchComics(limit: Int, skip: Int, searchQuery: String): List<ParseObject>
-
-    suspend fun fetchManga(limit: Int, skip: Int, searchQuery: String): List<ParseObject>
-
-    suspend fun addBook(item: BookItemDto)
+    suspend fun getBooks(offset: Long, limit: Long): List<BookItemDto>
 }
