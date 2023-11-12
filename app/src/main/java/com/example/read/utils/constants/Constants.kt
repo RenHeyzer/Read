604e052d07795842439674deafe11125ad61ac3b
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-12 19:38:11 +0600

    Added constants "BOOKS_TABLE_NAME"

--- app/src/main/java/com/example/read/utils/constants/Constants.kt
+++ app/src/main/java/com/example/read/utils/constants/Constants.kt
@@ -1,10 +1,11 @@
 package com.example.read.utils.constants
 
 object Constants {
-    const val RECOMMENDATIONS_CLASS_NAME = "Recommendations"
-    const val RECOMMENDATIONS_SLIDES_CLASS_NAME = "Slides"
-    const val COMICS_CLASS_NAME = "Comics"
-    const val MANGA_CLASS_NAME = "Manga"
-    const val INFO_CLASS_NAME = "Info"
-    const val CHAPTERS_CLASS_NAME = "Chapters"
+    const val BOOKS_TABLE_NAME = "books"
+    const val RECOMMENDATIONS_TABLE_NAME = "recommendations"
+    const val RECOMMENDATIONS_SLIDES_TABLE_NAME = "slides"
+    const val COMICS_TABLE_NAME = "comics"
+    const val MANGA_TABLE_NAME = "manga"
+    const val INFO_TABLE_NAME = "info"
+    const val CHAPTERS_TABLE_NAME = "chapters"
 }
