075b8d1916b97b352c84494cb62a0b4babf2696d
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-12 20:36:39 +0600

    Removed Parse initialization

--- app/src/main/java/com/example/read/App.kt
+++ app/src/main/java/com/example/read/App.kt
@@ -1,20 +1,7 @@
 package com.example.read
 
 import android.app.Application
-import com.parse.Parse
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        Parse.initialize(
-            Parse.Configuration.Builder(this)
-                .applicationId(getString(R.string.back4app_application_id))
-                .clientKey(getString(R.string.back4app_client_key))
-                .server(getString(R.string.back4app_server_url))
-                .build()
-        )
-    }
-}
+class App : Application()
--- app/src/main/java/com/example/read/feature_home/data/remote/dtos/RecommendationItemDto.kt
+++ app/src/main/java/com/example/read/feature_home/data/remote/dtos/RecommendationItemDto.kt
@@ -1,7 +1,6 @@
 package com.example.read.feature_home.data.remote.dtos
 
 import com.example.read.utils.mappers.Mappable
-import com.parse.ParseObject
 
 data class RecommendationItemDto(
     val id: String? = null,
@@ -9,10 +8,3 @@ data class RecommendationItemDto(
     val coverImage: String? = null,
     val posterImage: String? = null
 ): Mappable
-
-fun ParseObject.toRecommendationItemDto() = RecommendationItemDto(
-    id = objectId,
-    title = getString("title"),
-    coverImage = getString("cover_image"),
-    posterImage = getString("poster_image")
-)
